1834033e7219d4ee3b7ea303f324c3acfe0cb24e
Dr Firas Swidan <firas@localhost.localdomain>
2014-12-02 13:25:15 +0200

    adding composite food category

--- src/com/nutrinfomics/geneway/shared/FoodCategory.java
+++ src/com/nutrinfomics/geneway/shared/FoodCategory.java
@@ -1,7 +1,7 @@
 package com.nutrinfomics.geneway.shared;
 
 public enum FoodCategory {
-	MEAT, VEGETABLE_FRUIT, FISH, SEAFOOD, FRUIT, NUT, OIL, DAIRY, EGG, ROOT, LEGUME, VEGETABLE, CEREAL, OTHER, BAKED, SPICE;
+	MEAT, VEGETABLE_FRUIT, FISH, SEAFOOD, FRUIT, NUT, OIL, DAIRY, EGG, ROOT, LEGUME, VEGETABLE, CEREAL, OTHER, BAKED, SPICE, COMPOSITE;
 	
 	public static boolean isAnimal(FoodCategory foodCategory){
 		return foodCategory == MEAT || foodCategory == FISH || foodCategory == SEAFOOD;
