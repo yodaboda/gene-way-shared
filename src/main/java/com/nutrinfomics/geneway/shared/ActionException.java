e95ab0b3b8886e67327b75342d67b45e70923c62
Firas Swidan <frsswdn@gmail.com>
2018-12-28 17:15:40 +0200

    Make ExceptionType field final

--- src/main/java/com/nutrinfomics/geneway/shared/ActionException.java
+++ src/main/java/com/nutrinfomics/geneway/shared/ActionException.java
@@ -7,18 +7,15 @@ public class ActionException extends Exception implements Serializable {
   /** */
   private static final long serialVersionUID = 2721774147421559337L;
 
-  private ExceptionType exceptionType;
+  private final ExceptionType exceptionType;
 
   public ActionException(ExceptionType exceptionType) {
     super(exceptionType.toString());
-    setType(exceptionType);
+    this.exceptionType = exceptionType;
   }
 
   public ExceptionType getType() {
     return exceptionType;
   }
 
-  private void setType(ExceptionType exceptionType) {
-    this.exceptionType = exceptionType;
-  }
 }
