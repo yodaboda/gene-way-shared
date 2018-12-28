67aa6e40c8d640deacfea813bc8a99124543cfde
Firas Swidan <frsswdn@gmail.com>
2018-12-28 17:15:08 +0200

    Make ExceptionType extends Serializable

--- src/main/java/com/nutrinfomics/geneway/shared/ExceptionType.java
+++ src/main/java/com/nutrinfomics/geneway/shared/ExceptionType.java
@@ -1,5 +1,7 @@
 package com.nutrinfomics.geneway.shared;
 
-public interface ExceptionType {
+import java.io.Serializable;
+
+public interface ExceptionType extends Serializable{
   public String toString();
 }
