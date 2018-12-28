f640d66184e0915f70f53b308cda114cc2a1618a
Firas Swidan <frsswdn@gmail.com>
2018-12-28 18:10:36 +0200

    Logging exception and testing for them

--- src/test/java/com/nutrinfomics/geneway/shared/MeasurementUnitTest.java
+++ src/test/java/com/nutrinfomics/geneway/shared/MeasurementUnitTest.java
@@ -1,8 +1,17 @@
 package com.nutrinfomics.geneway.shared;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Handler;
+import java.util.logging.Level;
+import java.util.logging.LogRecord;
+import java.util.logging.Logger;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
@@ -10,67 +19,115 @@ import org.junit.rules.ExpectedException;
 
 import com.nutrinfomics.geneway.shared.testcategory.FastTest;
 
-@Category(value = {FastTest.class})
+@Category(value = { FastTest.class })
 public class MeasurementUnitTest {
 
-  @Rule public ExpectedException thrown = ExpectedException.none();
+	@Rule
+	public ExpectedException thrown = ExpectedException.none();
 
-  @Test
-  public void testParseString() {
-    assertEquals(MeasurementUnit.CUP, MeasurementUnit.parse(MeasurementUnit.CUP.toString()));
-  }
+	private Logger LOGGER = Logger.getLogger(MeasurementUnit.class.getName());
 
-  @Test
-  public void testParseStringG() {
-    assertEquals(MeasurementUnit.GRAM, MeasurementUnit.parse("g"));
-  }
+	public class LogHandler extends Handler {
+		private List<LogRecord> logRecords = new ArrayList<>();
 
-  @Test
-  public void testParseStringException() {
-    thrown.expect(IllegalArgumentException.class);
-    thrown.expectMessage("No enum constant com.nutrinfomics.geneway.shared.MeasurementUnit.A");
-    MeasurementUnit.parse("a");
-  }
+		@Override
+		public void publish(LogRecord record) {
+			logRecords.add(record);
+		}
 
-  @Test
-  public void testParseStringTrim() {
-    assertEquals(MeasurementUnit.MICROGRAM, MeasurementUnit.parse("�g "));
-  }
+		@Override
+		public void flush() {
+		}
 
-  @Test
-  public void testParseStringArrayInt() {
-    String[] nutrientUnits = new String[] {"g", MeasurementUnit.KCAL.toString(), "�g "};
-    int from = 1;
-    MeasurementUnit[] actualMeasurementUnits = MeasurementUnit.parse(nutrientUnits, from);
-    Assert.assertArrayEquals(
-        new MeasurementUnit[] {MeasurementUnit.KCAL, MeasurementUnit.MICROGRAM},
-        actualMeasurementUnits);
-  }
+		@Override
+		public void close() throws SecurityException {
+		}
 
-  @Test
-  public void testParseStringArrayIntException() {
-    String[] nutrientUnits = new String[] {"b ", MeasurementUnit.KCAL.toString(), "�g "};
-    int from = 0;
+		public List<LogRecord> getLogRecords() {
+			return logRecords;
+		}
+	}
 
-    thrown.expect(IllegalArgumentException.class);
-    thrown.expectMessage("No enum constant com.nutrinfomics.geneway.shared.MeasurementUnit.B");
+	private LogHandler logHandler = new LogHandler();
 
-    MeasurementUnit.parse(nutrientUnits, from);
-  }
+	@Before
+	public void setUp() {
+		LOGGER.addHandler(logHandler);
+	}
 
-  @Test
-  public void testParseStringArrayIntNull() {
-    String[] nutrientUnits = null;
-    int from = 1;
-    MeasurementUnit[] actualMeasurementUnits = MeasurementUnit.parse(nutrientUnits, from);
-    assertNull(actualMeasurementUnits);
-  }
+	@Test
+	public void testParseString() {
+		assertEquals(MeasurementUnit.CUP, MeasurementUnit.parse(MeasurementUnit.CUP.toString()));
+		assertEquals(0, logHandler.getLogRecords().size());
+	}
 
-  @Test
-  public void testParseStringArrayIntOutOfRange() {
-    String[] nutrientUnits = new String[] {"g", MeasurementUnit.KCAL.toString(), "�g "};
-    int from = 3;
-    MeasurementUnit[] actualMeasurementUnits = MeasurementUnit.parse(nutrientUnits, from);
-    assertNull(actualMeasurementUnits);
-  }
+	@Test
+	public void testParseStringG() {
+		assertEquals(MeasurementUnit.GRAM, MeasurementUnit.parse("g"));
+		assertEquals(0, logHandler.getLogRecords().size());
+	}
+
+	@Test
+	public void testParseStringException() {
+		try {
+			MeasurementUnit.parse("a");
+			fail("Expected exception of type IllegalArgumentException");
+		} catch (IllegalArgumentException ex) {
+			assertEquals("No enum constant com.nutrinfomics.geneway.shared.MeasurementUnit.A", ex.getMessage());
+			assertEquals(1, logHandler.getLogRecords().size());
+			assertEquals(Level.SEVERE, logHandler.getLogRecords().get(0).getLevel());
+
+		}
+	}
+
+	@Test
+	public void testParseStringTrim() {
+		assertEquals(MeasurementUnit.MICROGRAM, MeasurementUnit.parse("�g "));
+		assertEquals(0, logHandler.getLogRecords().size());
+	}
+
+	@Test
+	public void testParseStringArrayInt() {
+		String[] nutrientUnits = new String[] { "g", MeasurementUnit.KCAL.toString(), "�g " };
+		int from = 1;
+		MeasurementUnit[] actualMeasurementUnits = MeasurementUnit.parse(nutrientUnits, from);
+		Assert.assertArrayEquals(new MeasurementUnit[] { MeasurementUnit.KCAL, MeasurementUnit.MICROGRAM },
+				actualMeasurementUnits);
+		assertEquals(0, logHandler.getLogRecords().size());
+	}
+
+	@Test
+	public void testParseStringArrayIntException() {
+		String[] nutrientUnits = new String[] { "b ", MeasurementUnit.KCAL.toString(), "�g " };
+		int from = 0;
+
+		try {
+			MeasurementUnit.parse(nutrientUnits, from);
+			fail("Expceted exception of type IllegalArgumentException");
+		} catch (IllegalArgumentException ex) {
+			assertEquals("No enum constant com.nutrinfomics.geneway.shared.MeasurementUnit.B", ex.getMessage());
+			assertEquals(1, logHandler.getLogRecords().size());
+			assertEquals(Level.SEVERE, logHandler.getLogRecords().get(0).getLevel());
+
+		}
+
+	}
+
+	@Test
+	public void testParseStringArrayIntEmpty() {
+		String[] nutrientUnits = null;
+		int from = 1;
+		MeasurementUnit[] actualMeasurementUnits = MeasurementUnit.parse(nutrientUnits, from);
+		assertEquals(0, actualMeasurementUnits.length);
+		assertEquals(0, logHandler.getLogRecords().size());
+	}
+
+	@Test
+	public void testParseStringArrayIntOutOfRange() {
+		String[] nutrientUnits = new String[] { "g", MeasurementUnit.KCAL.toString(), "�g " };
+		int from = 3;
+		MeasurementUnit[] actualMeasurementUnits = MeasurementUnit.parse(nutrientUnits, from);
+		assertEquals(0, actualMeasurementUnits.length);
+		assertEquals(0, logHandler.getLogRecords().size());
+	}
 }
